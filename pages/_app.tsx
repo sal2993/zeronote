3ec6139ce289795fe0fcaf73e8b7ca20446b8b33
sal camara <sal2993@hotmail.com>
2023-07-10 08:57:53 -0600

    trying to get rid of nextui..

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { UserProvider } from '@auth0/nextjs-auth0'
-import { createTheme, NextUIProvider } from '@nextui-org/react';
 
 
-const theme = createTheme({
-  type: "dark",
-  theme: {
-    colors: {
-      text: "#789"
-    },
-  }
-})
 
 function MyApp({ Component, pageProps }: AppProps) {
 
 
   return (
-    <NextUIProvider theme={theme}>
       <UserProvider>
         <Component {...pageProps} />
       </UserProvider>
-    </NextUIProvider>
   )
 }
 
